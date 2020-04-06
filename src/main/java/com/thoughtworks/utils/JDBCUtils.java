be8b7554edd88126c02f5c7bf8c086f0f726d8da
Edwardlin-zlt <edwardlin.zlt@gmail.com>
2020-04-06 16:22:11 +0800

    refact

--- src/main/java/com/thoughtworks/utils/JDBCUtils.java
+++ src/main/java/com/thoughtworks/utils/JDBCUtils.java
@@ -1,44 +1,45 @@
 package com.thoughtworks.utils;
 
-import java.io.FileReader;
 import java.io.IOException;
-import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Objects;
 import java.util.Properties;
 
 public class JDBCUtils {
     private static String url; // 要不要加final? TODO
     private static String user;
     private static String password;
-    private static String driver;
 
     static {
         try {
             Properties properties = new Properties();
             ClassLoader classLoader = JDBCUtils.class.getClassLoader();
-            URL resource = classLoader.getResource("jdbcsettings.properties");
-            properties.load(new FileReader(resource.getPath()));
+            properties.load(classLoader.getResourceAsStream("jdbcsettings.properties"));
 
             url = properties.getProperty("url");
             user = properties.getProperty("user");
             password = properties.getProperty("password");
-            driver = properties.getProperty("driver");
-            Class.forName(driver);
-        } catch (IOException | ClassNotFoundException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static Connection getConnection() throws SQLException { // 数据库连错了，基本就要终止程序了
+    public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(url, user, password);
     }
 
-    public static void close(Statement stmt, Connection conn) {
+    public static void close(ResultSet rs, Statement stmt, Connection conn) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
         if (conn != null) {
             try {
                 conn.close();
@@ -46,22 +47,13 @@ public class JDBCUtils {
                 e.printStackTrace();
             }
         }
+
         if (stmt != null) {
             try {
                 stmt.close();
             } catch (SQLException e) {
                 e.printStackTrace();
             }
-        }
-    }
-
-    public static void close(Statement stmt, Connection conn, ResultSet resultset) {
-        try {
-            Objects.requireNonNull(stmt).close();
-            Objects.requireNonNull(conn).close();
-            Objects.requireNonNull(resultset).close();
-        } catch (SQLException e) {
-            e.printStackTrace();
         }
     }
 }
